56964311b0045ac080c9afb9741a59816df9c1b1
Kruzenger <33284905+Kruzenger@users.noreply.github.com>
2018-12-20 22:04:19 +0300

    Add files via upload

---
+++ main_character/move_sprites_main_character.js
@@ -0,0 +1,477 @@
+
+
+
+
+var main_character = document.getElementById('main_character');
+var BG = document.getElementById('BG');
+var Stamina = document.getElementById('Stamina_bar');
+var Health = document.getElementById('HP_bar');
+var game = document.getElementById('Game');
+
+var cadr_wight_right = 1250;
+const cadr_height_right = 0;
+var cadr_wight_left = 1100;
+const cadr_height_left = 0;
+
+var cadr_wight_stay_right = 1250;
+var cadr_wight_stay_left = 350;
+const cadr_height_stay = 150;
+
+var cadr_charge_wight_right = 1250;
+const cadr_charge_height_right = 300;
+var cadr_charge_wight_left = 1000;
+const cadr_charge_height_left = 300;
+
+var	Xposition = -250;
+var side = 1;
+var stay_player = 1;
+
+var cadr_wight_right_sheld = 2150;
+var cadr_wight_left_sheld = 200;
+const cadr_sheld_height = 150;
+
+var cadr_hit_width_right = 1250;
+var cadr_hit_width_left = 1100;
+const cadr_hit_height = 450;
+
+var my_pain_right_X = 1250;
+var my_pain_left_X = 1100;
+const my_pain_Y = 600;
+
+var BG_Xposition = 0;
+
+var Stamina = document.getElementById('Stamina_bar');
+var which_fun = 0;
+
+var Couldness = 1;
+var cooldown = 1;
+
+var shield_up = 0;
+
+var press = 0;
+
+var me_stun = 0;
+
+
+
+
+
+
+is_moob_alive1();
+
+is_moob_alive2();
+
+searching1();
+
+searching2();
+
+am_I_alive();
+
+Stamina_fulling();
+
+
+
+
+
+
+addEventListener("keydown", WAlking);
+
+function WAlking(e){
+	if (me_stun == 0) {
+		switch(e.keyCode){
+         
+       		case 65:    // если нажата клавиша A
+       			stay_player = 0;
+        		Couldness = 0;
+        		press = 1;
+        		side = 0;
+        		walk();
+          		break;
+
+       		case 68:  	// если нажата клавиша D
+        		stay_player= 0;
+        		Couldness = 0;
+        		press = 1;
+        		side = 1; 
+           		walk();
+            	break;
+
+        	case 16: 	// если нажата клавиша Shift
+        		if (Stamina_bar.value >= 40 && Couldness == 1 && cooldown == 1) {
+        			cooldown = 0;
+ 					setTimeout(function(){cooldown = 1;}, 650);
+        			press = 1;
+        			Stamina_bar.value -= 40;
+					main_character.style.width = '250px';
+        			charge();
+        			break;
+        		}
+
+ 			case 83:    // если нажата клавиша S
+ 				shield_up = 1;
+ 				stay_player = 1;
+ 				Couldness = 1;
+ 				press = 1;
+ 				shield();
+ 				break;
+
+ 			case 81: 	// если нажата клавиша Q
+ 				if (Stamina_bar.value >= 30 && cooldown == 1) {
+ 					cooldown = 0;
+ 					setTimeout(function(){cooldown = 1;}, 500);
+ 					press = 1;
+ 					Stamina_bar.value -= 30; 
+ 					hit();
+ 					break;
+ 				}
+ 		}
+    }
+}
+
+addEventListener("keyup", function(){
+	stay_player = 1;
+	press = 0; 
+	shield_up = 0;
+	Couldness = 1; 
+	setTimeout(check_staying, 400)
+});
+
+function check_staying(){
+	if (press == 1) {
+	
+	}
+	else{
+		staying();
+	}
+}
+
+function staying(){
+	stay_here();
+}
+
+function repmake_150px(){
+	main_character.style.width = '150px';
+}
+
+function stay_here(){
+	repmake_150px();
+	cadr_wight_right = 1250;
+	cadr_wight_left = 1100;
+
+	sheld = 0;
+
+	if (side == 1) {
+		main_character.style.backgroundPositionX = '-' + cadr_wight_stay_right + 'px';
+		main_character.style.backgroundPositionY = '-' + cadr_height_stay + 'px';
+	}
+	else{
+		main_character.style.backgroundPositionX = '-' + cadr_wight_stay_left + 'px';
+		main_character.style.backgroundPositionY = '-' + cadr_height_stay + 'px';
+	}
+}
+
+function walk(){
+	if (side == 1) {
+		main_character.style.backgroundPositionX = '-' + cadr_wight_right + 'px';
+		main_character.style.backgroundPositionY = '-' + cadr_height_right + 'px';
+
+		if (Xposition >= 100) {
+			BG_Xposition -= 10;
+			if (((enemy_position1 - Xposition) > -20 && (enemy_position1 - Xposition) < 0) || enemy_atack1 == 0) {
+				enemy_position1 -= 10;
+				enemy1.style.left = enemy_position1 + 'px';
+			}
+			if (((enemy_position2 - Xposition) > -20 && (enemy_position2 - Xposition) < 0) || enemy_atack2 == 0) {
+				enemy_position2 -= 10;
+				enemy2.style.left = enemy_position2 + 'px';
+			}
+			BG.style.backgroundPositionX = BG_Xposition + 'px';
+		}
+		else{
+			Xposition += 13;
+			main_character.style.left = Xposition + 'px';
+		}
+
+		if (cadr_wight_right == 2300) {
+			cadr_wight_right = 1250;
+		}
+		else{
+			cadr_wight_right += 150;
+		}
+		side = 1;
+	}
+	else{
+		main_character.style.backgroundPositionX = '-' + cadr_wight_left + 'px';
+		main_character.style.backgroundPositionY = '-' + cadr_height_left + 'px';
+
+		if (Xposition <= -350) {
+			BG_Xposition += 10;
+			if (((enemy_position1 - Xposition) < 20 && (enemy_position1 - Xposition) > 0) || enemy_atack1 == 0) {
+				enemy_position1 += 10;
+				enemy1.style.left = enemy_position1 + 'px';
+			}
+			if (((enemy_position2 - Xposition) < 20 && (enemy_position2 - Xposition) > 0) || enemy_atack2 == 0) {
+				enemy_position2 += 10;
+				enemy2.style.left = enemy_position2 + 'px';
+			}
+			BG.style.backgroundPositionX = BG_Xposition + 'px';
+		}	
+		else{
+			Xposition -= 13;
+			main_character.style.left = Xposition + 'px';
+		}
+
+		if (cadr_wight_left == 50) {
+			cadr_wight_left = 1100;
+		}
+		else{
+			cadr_wight_left -= 150;
+		}
+		side = 0;
+	}
+}
+
+function charge(){
+
+	if (side == 1) {
+
+		main_character.style.backgroundPositionX = '-' + cadr_charge_wight_right + 'px';
+		main_character.style.backgroundPositionY = '-' + cadr_charge_height_right + 'px';
+
+		if (Xposition >= 100) {
+			BG_Xposition -= 65;
+			enemy_position1 -= 65;
+			enemy_position2 -= 65;
+			enemy2.style.left = enemy_position2 + 'px';
+			enemy1.style.left = enemy_position1 + 'px';
+			BG.style.backgroundPositionX = BG_Xposition + 'px';
+		}
+		else{
+			Xposition += 65;
+			main_character.style.left = Xposition + 'px';
+		}
+
+		if(cadr_charge_wight_right != 2250){
+			cadr_charge_wight_right += 250;
+			setTimeout(charge, 50);
+		}
+		else{
+			if(enemy_side1 == 1 && (enemy_position1 - Xposition) >= -10 && (enemy_position1 - Xposition) <= 100){
+				enemy_atack1 = 1;
+				enemy_health_bar1.value -= 40;
+				enemy_pain1();
+			}
+			if (enemy_side2 == 1 && (enemy_position2 - Xposition) >= -10 && (enemy_position2 - Xposition) <= 100) {
+				enemy_atack2 = 1;
+				enemy_health_bar2.value -= 40;
+				enemy_pain2();
+			}
+			if (enemy_side1 == 0 && (enemy_position1 - Xposition) >= -20 && (enemy_position1 - Xposition) <= 80) {
+				enemy_atack1 = 1;
+				enemy_health_bar1.value -= 40;
+				enemy_pain1();
+			}
+			if (enemy_side2 == 0 && (enemy_position2 - Xposition) >= -20 && (enemy_position2 - Xposition) <= 80) {
+				enemy_atack2 = 1;
+				enemy_health_bar2.value -= 40;
+				enemy_pain2();
+			}
+
+			setTimeout(repmake_150px, 50);
+			cadr_charge_wight_right = 1250;
+			setTimeout(stay_here, 50);
+		}
+	}
+	else{
+		main_character.style.backgroundPositionX = '-' + cadr_charge_wight_left + 'px';
+		main_character.style.backgroundPositionY = '-' + cadr_charge_height_left + 'px';
+
+		if (Xposition <= -350) {
+			BG_Xposition += 65;
+			enemy_position1 += 65;
+			enemy_position2 += 65;
+			enemy2.style.left = enemy_position2 + 'px';
+			enemy1.style.left = enemy_position1 + 'px';
+			BG.style.backgroundPositionX = BG_Xposition + 'px';
+
+		}	
+		else{
+			Xposition -= 65;
+			main_character.style.left = Xposition + 'px';
+		}
+
+		if(cadr_charge_wight_left != 0){
+			cadr_charge_wight_left -= 250;
+			setTimeout(charge, 50);
+		}
+		else{
+			if(enemy_side1 == 0 && (enemy_position1 - Xposition) <= 10 && (enemy_position1 - Xposition) >= -100){
+				enemy_atack1 = 1;
+				enemy_health_bar1.value -= 40;
+				enemy_pain1();
+			}
+			if (enemy_side2 == 0 && (enemy_position2 - Xposition) <= 10 && (enemy_position2 - Xposition) >= -100) {
+				enemy_atack2 = 1;
+				enemy_health_bar2.value -= 40;
+				enemy_pain2();
+			}
+			
+			if (enemy_side1 == 1 && (enemy_position1 - Xposition) <= 40 && (enemy_position1 - Xposition) >= -80) {
+				enemy_atack1 = 1;
+				enemy_health_bar1.value -= 40;
+				enemy_pain1();
+			}
+			if (enemy_side1 == 2 && (enemy_position2 - Xposition) <= 40 && (enemy_position2 - Xposition) >= -80) {
+				enemy_atack2 = 1;
+				enemy_health_bar2.value -= 40;
+				enemy_pain2();
+			}
+			setTimeout(repmake_150px, 50);
+			cadr_charge_wight_left = 1000;
+			setTimeout(stay_here, 50);
+		}
+	}
+}
+
+function shield(){
+	if (side == 1) {
+		main_character.style.backgroundPositionX = '-' + cadr_wight_right_sheld + 'px';
+		main_character.style.backgroundPositionY = '-' + cadr_sheld_height + 'px';
+	}
+	else{
+		main_character.style.backgroundPositionX = '-' + cadr_wight_left_sheld + 'px';
+		main_character.style.backgroundPositionY = '-' + cadr_sheld_height + 'px';
+	}
+}
+
+function hit(){
+	if (side == 1) {
+
+		main_character.style.backgroundPositionX = '-' + cadr_hit_width_right + 'px';
+		main_character.style.backgroundPositionY = '-' + cadr_hit_height + 'px';
+
+		if(cadr_hit_width_right < 1850){
+			cadr_hit_width_right += 150;
+			setTimeout(hit, 50);
+		}
+		else{
+			if (cadr_hit_width_right < 2050) {
+				main_character.style.width = '200px';
+				cadr_hit_width_right += 200;
+				setTimeout(hit, 50);
+			}
+			else{
+				if(enemy_side1 == 1 && (enemy_position1 - Xposition) >= 0 && (enemy_position1 - Xposition) <= 100){
+					enemy_atack1 = 1;
+					enemy_health_bar1.value -= 40;
+					enemy_pain1();
+				}
+				if (enemy_side2 == 1 && (enemy_position2 - Xposition) >= 0 && (enemy_position2 - Xposition) <= 100) {
+					enemy_atack2 = 1;
+					enemy_health_bar2.value -= 40;
+					enemy_pain2();
+				}
+				if (enemy_side1 == 0 && (enemy_position1 - Xposition) >= -20 && (enemy_position1 - Xposition) <= 60) {
+					enemy_atack1 = 1;
+					enemy_health_bar1.value -= 40;
+					enemy_pain1();
+				}
+				if (enemy_side2 == 0 && (enemy_position2 - Xposition) >= -20 && (enemy_position2 - Xposition) <= 60) {
+					enemy_atack2 = 1;
+					enemy_health_bar2.value -= 40;
+					enemy_pain2();
+				}
+				setTimeout(repmake_150px, 150);
+				cadr_hit_width_right = 1250;
+				setTimeout(stay_here, 150);
+			}
+		}
+	}
+	else{
+		main_character.style.backgroundPositionX = '-' + cadr_hit_width_left + 'px';
+		main_character.style.backgroundPositionY = '-' + cadr_hit_height + 'px';
+
+		if (cadr_hit_width_left > 500) {
+			cadr_hit_width_left -= 150;
+			setTimeout(hit, 50);
+		}
+		else{
+			if(cadr_hit_width_left > 100 && cadr_hit_width_left < 500){
+				main_character.style.width = '200px';
+				cadr_hit_width_left -= 200;
+				setTimeout(hit, 50);
+			}
+			else{
+				if(enemy_side1 == 0 && (enemy_position1 - Xposition) <= 0 && (enemy_position1 - Xposition) >= -80){
+					enemy_atack1 = 1;
+					enemy_health_bar1.value -= 40;
+					enemy_pain1();
+				}
+				if (enemy_side2 == 0 && (enemy_position2 - Xposition) <= 0 && (enemy_position2 - Xposition) >= -80) {
+					enemy_atack2 = 1;
+					enemy_health_bar2.value -= 40;
+					enemy_pain2();
+				}
+				
+				if (enemy_side1 == 1 && (enemy_position1 - Xposition) <= 20 && (enemy_position1 - Xposition) >= -60) {
+					enemy_atack1 = 1;
+					enemy_health_bar1.value -= 40;
+					enemy_pain1();
+				}
+				if (enemy_side2 == 1 && (enemy_position2 - Xposition) <= 20 && (enemy_position2 - Xposition) >= -60) {
+					enemy_atack2 = 1;
+					enemy_health_bar2.value -= 40;
+					enemy_pain2();
+				}
+				
+				setTimeout(repmake_150px, 150);
+				cadr_hit_width_left = 1100;
+				setTimeout(stay_here, 150);
+			}
+		}
+	}
+}
+
+function my_pain(){
+	me_stun = 1;
+	if(side == 1){
+		main_character.style.backgroundPositionX = '-' + my_pain_right_X + 'px';
+		main_character.style.backgroundPositionY = '-' + my_pain_Y + 'px';
+
+		if (my_pain_right_X == 1550) {
+			my_pain_right_X = 1250;
+			me_stun = 0;
+		}
+		else{
+			my_pain_right_X += 150;
+			setTimeout(my_pain, 100); 
+		}
+	}
+	else{
+		main_character.style.backgroundPositionX = '-' + my_pain_left_X + 'px';
+		main_character.style.backgroundPositionY = '-' + my_pain_Y + 'px';
+
+		if (my_pain_left_X == 800) {
+			my_pain_left_X = 1100;
+			me_stun = 0;
+		}
+		else{
+			my_pain_left_X -= 150;
+			setTimeout(my_pain, 100); 
+		}
+	}
+}
+
+function Stamina_fulling(){
+	Stamina_bar.value += 1;
+	setTimeout(Stamina_fulling, 75);
+}
+
+
+
+function am_I_alive(){
+	if (Health.value <= 0) {
+		location.reload();
+	}
+	setTimeout(am_I_alive, 50);
+}
---
+++ main_character/move_sprites_moob_1.js
@@ -0,0 +1,370 @@
+
+
+
+
+
+var enemy1 = document.getElementById('enemy1');
+var enemy_health_bar1 = document.getElementById('Health_of_enemy1');
+var spear1 = document.getElementById('spear1');
+
+var enemy_position1 = 300;
+var enemy_side1 = 0;
+var stay_enemy1 = 1;
+var atacking1 = 1;
+var enemy_atack1 = 0;
+var throwing1 = 1;
+var spear_position1 = 0;
+var stun1 = 0;
+var dead1 = 0;
+
+
+
+var enemy_stay_right_X = 1250;
+var enemy_stay_left_X = 1100;
+const enemy_stay_Y = 150;
+
+var enemy_run_right_X = 1250;
+var enemy_run_left_X = 1100;
+const enemy_run_Y = 0;
+
+var enemy_atack_with_spear_right_X = 1250;
+var enemy_atack_with_spear_left_X = 1100;
+const enemy_atack_with_spear_Y = 300;
+
+var enemy_atack_right_X = 1250;
+var enemy_atack_left_X = 1100;
+const enemy_atack_Y = 450;
+
+var enemy_pain_right_X = 1250;
+var enemy_pain_left_X = 1100;
+const enemy_pain_Y = 600;
+
+
+
+
+
+
+function searching1(){
+	if (dead1 == 0) {
+		if (enemy_atack1 == 0) {
+			if (enemy_side1 == 1) {
+				enemy_health_bar1.style.left = '0px';
+				if ((enemy_position1 - Xposition) < 0 && (enemy_position1 - Xposition) >= -500) {
+					if (stun1 == 0) {
+						enemy_run_and_atack1();
+					}
+				}
+				else{
+					if (stay_enemy1 == 1) {
+						enemy_stay1();
+					}
+				}
+			}
+			else{
+				if ((enemy_position1 - Xposition) > 0 && (enemy_position1 - Xposition) <= 500) {
+					if (stun1 == 0) {
+						enemy_run_and_atack1();
+					}
+				}
+				else{
+					enemy_health_bar1.style.left = '40px';
+					if (stay_enemy1 == 1) {
+						enemy_stay1();
+					}
+				}
+			}
+			setTimeout(searching1, 50);
+		}
+		else{
+			if (stun1 == 0) {
+				enemy_run_and_atack1();
+			}
+			setTimeout(searching1, 50);
+		}
+	}
+}
+
+
+function enemy_stay1(){
+	stay_enemy1 = 0;
+	if (enemy_side1 == 1) {
+		enemy_side1 = 0;
+		enemy1.style.backgroundPositionX = '-' + enemy_stay_left_X + 'px';
+		enemy1.style.backgroundPositionY = '-' + enemy_stay_Y + 'px';
+	} 
+	else{
+		enemy_side1 = 1;
+		enemy1.style.backgroundPositionX = '-' + enemy_stay_right_X + 'px';
+		enemy1.style.backgroundPositionY = '-' + enemy_stay_Y + 'px';	
+	}
+	setTimeout(change_stay1, 5000);
+}
+
+function change_stay1(){
+	stay_enemy1 = 1;
+}
+
+function enemy_run_and_atack1(){
+	enemy_atack1 = 1;
+	if (enemy_side1 == 1) {
+		enemy_health_bar1.style.left = '0px';
+		if (((enemy_position1 - Xposition) >= -400 && (enemy_position1 - Xposition) <= -60)) {
+			enemy_run1();
+		}
+		else if((enemy_position1 - Xposition) >= -500 && (enemy_position1 - Xposition) <= -400 && throwing1 == 1 && stay_player == 1){
+			enemy1.style.backgroundPositionX = '-' + enemy_atack_with_spear_right_X + 'px';
+			enemy1.style.backgroundPositionY = '-' + enemy_atack_with_spear_Y + 'px';
+
+			if (enemy_atack_with_spear_right_X == 1850) {
+				create_spear1();
+			}
+
+			if (enemy_atack_with_spear_right_X == 2300) {
+				enemy_atack_with_spear_right_X = 1250;
+				throwing1 = 0;
+			}
+			else{
+				enemy_atack_with_spear_right_X += 150;
+			}
+		}
+		else if((enemy_position1 - Xposition) >= -500 && (enemy_position1 - Xposition) <= -400 && throwing1 == 0 && stay_player == 1){
+			enemy_stay_right1();
+		}
+		else if ((enemy_position1 - Xposition) >= -500 && (enemy_position1 - Xposition) <= -400 && stay_player == 0) {
+			enemy_run1();
+		}
+		else if ((enemy_position1 - Xposition) < -500) {
+			enemy_run1();
+		}
+		else if ((enemy_position1 - Xposition) <= 0 && (enemy_position1 - Xposition) >= -60 && stay_player == 1 && atacking1 == 1) {
+			atacking1 = 0;
+			atack1();
+		}
+		else if((enemy_position1 - Xposition) <= 0 && (enemy_position1 - Xposition) >= -60 && stay_player == 0){
+			enemy_run1();
+		}
+		else if((enemy_position1 - Xposition) < 20 && (enemy_position1 - Xposition) > 0 && atacking1 == 1){
+			atacking1 = 0;
+			atack1();
+		}
+		else if ((enemy_position1 - Xposition) >= 20){
+			enemy_side1 = 0;
+		}
+	}
+	////////////////////////////////////////////////////////////////////////////////////////
+
+	if (enemy_side1 == 0) {
+		enemy_health_bar1.style.left = '40px';
+		if ((enemy_position1 - Xposition) <= 400 && (enemy_position1 - Xposition) >= 60) {
+			enemy_run1();
+		}
+		else if((enemy_position1 - Xposition) <= 500 && (enemy_position1 - Xposition) > 400 && throwing1 == 1 && stay_player == 1){
+			enemy1.style.backgroundPositionX = '-' + enemy_atack_with_spear_left_X + 'px';
+			enemy1.style.backgroundPositionY = '-' + enemy_atack_with_spear_Y + 'px';
+
+			if (enemy_atack_with_spear_left_X == 650) {
+				create_spear1();
+			}
+
+			if (enemy_atack_with_spear_left_X <= 50) {
+				enemy_atack_with_spear_left_X = 1100;
+				throwing1 = 0;
+			}
+			else{
+				enemy_atack_with_spear_left_X -= 150;
+			}
+		}
+		else if((enemy_position1 - Xposition) <= 500 && (enemy_position1 - Xposition) >= 400 && throwing1 == 0 && stay_player == 1){
+			enemy_stay_left1();
+		}
+		else if ((enemy_position1 - Xposition) <= 500 && (enemy_position1 - Xposition) >= 400 && stay_player == 0) {
+			enemy_run1();
+		}
+		else if ((enemy_position1 - Xposition) > 500) {
+			enemy_run1();
+		}
+		else if ((enemy_position1 - Xposition) >= 0 && (enemy_position1 - Xposition) <= 60 && stay_player == 1 && atacking1 == 1) {
+			atacking1 = 0;
+			atack1();
+		}
+		else if((enemy_position1 - Xposition) >= 0 && (enemy_position1 - Xposition) <= 60 && stay_player == 0){
+			enemy_run1();
+		}
+		else if((enemy_position1 - Xposition) > -20 && (enemy_position1 - Xposition) < 0 && atacking1 == 1){
+			atacking1 = 0;
+			atack1();
+		}
+		else if((enemy_position1 - Xposition) <= -20){
+			enemy_side1 = 1;
+		}
+	}
+}
+
+
+function atack1(){
+	if (enemy_side1 == 1) {
+		enemy1.style.backgroundPositionX = '-' + enemy_atack_right_X + 'px';
+		enemy1.style.backgroundPositionY = '-' + enemy_atack_Y + 'px';
+
+		if (enemy_atack_right_X == 2000) {
+			enemy_atack_right_X = 1250;
+			if (shield_up == 0) {
+				Health.value -= 40;
+				my_pain();
+			}
+			setTimeout(function(){atacking1 = 1;}, 1000);
+		}
+		else{
+			enemy_atack_right_X += 150;
+			setTimeout(atack1, 70);
+		}
+	}
+	if (enemy_side1 == 0) {
+		enemy1.style.backgroundPositionX = '-' + enemy_atack_left_X + 'px';
+		enemy1.style.backgroundPositionY = '-' + enemy_atack_Y + 'px';
+
+		if (enemy_atack_left_X <= 350) {
+			enemy_atack_left_X = 1100;
+			if (shield_up == 0) {
+				Health.value -= 40;
+				my_pain();
+			}
+			setTimeout(function(){atacking1 = 1;}, 1000);
+		}
+		else{
+			enemy_atack_left_X -= 150;
+			setTimeout(atack1, 70);
+		}
+	}
+}
+
+
+
+
+function enemy_run1(){
+	if (enemy_side1 == 1) {
+		enemy1.style.backgroundPositionX = '-' + enemy_run_right_X + 'px';
+		enemy1.style.backgroundPositionY = '-' + enemy_run_Y + 'px';
+
+		if (Xposition <= 100 || stay_player == 1 || side == 0) {
+			enemy_position1 += 10;
+			enemy1.style.left = enemy_position1 + 'px';
+		}
+
+		if (enemy_run_right_X >= 2300) {
+			enemy_run_right_X = 1250;
+		}
+		else{
+			enemy_run_right_X += 150;
+		}
+	}
+	if (enemy_side1 == 0) {
+		enemy1.style.backgroundPositionX = '-' + enemy_run_left_X + 'px';
+		enemy1.style.backgroundPositionY = '-' + enemy_run_Y + 'px';
+
+		if (Xposition >= -350 || stay_player == 1 || side == 1) {
+			enemy_position1 -= 10;
+			enemy1.style.left = enemy_position1 + 'px';
+		}
+
+		if (enemy_run_left_X <= 50) {
+			enemy_run_left_X = 1100;
+		}
+		else{
+			enemy_run_left_X -= 150;
+		}
+	}
+}
+
+function enemy_stay_right1(){
+	enemy_health_bar1.style.left = '0px';
+	enemy1.style.backgroundPositionX = '-' + enemy_stay_right_X + 'px';
+	enemy1.style.backgroundPositionY = '-' + enemy_stay_Y + 'px';
+}
+
+function enemy_stay_left1(){
+	enemy_health_bar1.style.left = '40px';
+	enemy1.style.backgroundPositionX = '-' + enemy_stay_left_X + 'px';
+	enemy1.style.backgroundPositionY = '-' + enemy_stay_Y + 'px';
+}
+
+function create_spear1(){
+	spear_position1 = enemy_position1;
+	spear1.style.left = spear_position1 + 'px';
+	mooving_spear1();
+}
+
+function mooving_spear1(){
+	if (enemy_side1 == 1) {
+		spear1.style.backgroundPositionY = '0px';
+		if (spear_position1 < (Xposition - 35)) {
+			spear_position1 += 5;
+			spear1.style.left = spear_position1 + 'px';
+			setTimeout(mooving_spear1, 5);
+		}
+		else{
+			if (shield_up == 0) {
+				Health.value -= 25;
+				my_pain();
+			}
+			setTimeout(function(){throwing1 = 1}, 750);
+			spear1.style.left = '-1100px';
+		}
+	}
+	else{
+		spear1.style.backgroundPositionY = '-15px';
+		if (spear_position1 > (Xposition + 35)) {
+			spear_position1 -= 5;
+			spear1.style.left = spear_position1 + 'px';
+			setTimeout(mooving_spear1, 5);
+		}
+		else{
+			if (shield_up == 0) {
+				Health.value -= 25;
+				my_pain();
+			}
+			setTimeout(function(){throwing1 = 1}, 750);
+			spear1.style.left = '-1100px';
+		}
+	}	
+}
+
+function enemy_pain1(){
+	stun1 = 1;
+	if(side == 0){
+		enemy1.style.backgroundPositionX = '-' + enemy_pain_right_X + 'px';
+		enemy1.style.backgroundPositionY = '-' + enemy_pain_Y + 'px';
+
+		if (enemy_pain_right_X == 1550) {
+			enemy_pain_right_X = 1250;
+			setTimeout(function(){stun1 = 0;}, 50);
+		}
+		else{
+			enemy_pain_right_X += 150;
+			setTimeout(enemy_pain1, 150); 
+		}
+	}
+	else{
+		enemy1.style.backgroundPositionX = '-' + enemy_pain_left_X + 'px';
+		enemy1.style.backgroundPositionY = '-' + enemy_pain_Y + 'px';
+
+		if (enemy_pain_left_X == 800) {
+			enemy_pain_left_X = 1100;
+			setTimeout(function(){stun1 = 0;}, 50);
+		}
+		else{
+			enemy_pain_left_X -= 150;
+			setTimeout(enemy_pain1, 150); 
+		}
+	}
+}
+
+
+function is_moob_alive1(){
+	if (enemy_health_bar1.value <= 0) {
+		dead1 = 1;
+		enemy1.style.opacity = '0';
+		setTimeout(function(){enemy1.style.top = '0px'}, 3000)
+	}
+	setTimeout(is_moob_alive1, 50);
+}
